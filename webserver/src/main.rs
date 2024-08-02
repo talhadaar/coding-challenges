578ec418f3ee54fa99c7c65cad3a9cc3d04601ed
talhadaar <muhammadtalhadar@gmail.com>
2024-08-03 00:03:36 +0500

    implemented some api

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,7 +1,10 @@
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 use tokio::{
     io::AsyncWriteExt,
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream}, task::JoinSet,
 };
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -9,18 +12,76 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 pub const ADDR: [u8; 4] = [127, 0, 0, 1];
 pub const PORT: u16 = 8080;
 
+#[derive(Debug)]
+struct Client {
+    stream: TcpStream,
+    saddr: SocketAddr,
+}
+
+#[derive(Debug, Clone)]
+struct WebServer {
+    ssocket: Arc<TcpListener>,
+    cpool: Arc<Mutex<Vec<Client>>>,
+    termination_flag: Arc<Mutex<bool>>,
+}
+
+impl WebServer {
+    async fn new(saddr: &SocketAddr) -> Result<Self> {
+        let ssocket = Arc::new(TcpListener::bind(saddr).await?);
+        let cpool = Arc::new(Mutex::new(Vec::new()));
+        Ok(Self { ssocket, cpool , termination_flag: Arc::new(Mutex::new(false))})
+    }
+}
+
+async fn processor(ssocket: Arc<TcpListener>, cpool: Arc<Mutex<Vec<Client>>>) -> Result<()>{
+    unimplemented!()
+}
+
+async fn pool_clients(ssocket: Arc<TcpListener>, cpool: Arc<Mutex<Vec<Client>>>) -> Result<()>{
+    unimplemented!()
+}
+
+async fn handle_client(client: &mut Client) -> Result<()> {
+    unimplemented!()
+}
+
+async fn handle_pool_clients(cpool: Arc<Mutex<Vec<Client>>>) -> Result<()> {
+    unimplemented!()
+}
+
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let listener = TcpListener::bind(SocketAddr::from((ADDR, PORT))).await?;
+    let server_socket = SocketAddr::from((ADDR, PORT));
+    let webserver = WebServer::new(&server_socket).await?;
 
-    loop {
-        let (socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            if let Err(e) = handle_connection(socket).await {
-                eprintln!("Error: {:?}", e);
-            }
-        });
-    }
+    let mut set = JoinSet::new();
+
+    println!("-> Setting up listening");
+    let ssocket = Arc::clone(&webserver.ssocket);
+    let cpool = Arc::clone(&webserver.cpool);
+
+    set.spawn(async move{
+        let _ = processor(ssocket, cpool).await;
+    });
+
+    println!("-> Setting up client handeling");
+    let cpool = Arc::clone(&webserver.cpool);
+    set.spawn(async move {
+        let _ = handle_pool_clients(cpool).await;
+    });
+
+    let termination_flag = Arc::clone(&webserver.termination_flag);
+    ctrlc_async::set_async_handler(async move {
+        println!("-> Server terminated");
+        let mut lock = termination_flag.lock().unwrap();
+        *lock = true;
+    })?;
+
+    set.join_next().await;
+    set.join_next().await;
+
+    Ok(())
 }
 
 async fn handle_connection(mut stream: TcpStream) -> Result<()> {
